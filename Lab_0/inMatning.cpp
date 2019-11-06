c69035f9a74f7fbdc18efe1ce6a4d9986ee9d908
Kenneth <kenbo736@student.liu.se>
2019-11-06 22:25:00 +0100

    char skriv tecken

--- Lab_0/inMatning.cpp
+++ Lab_0/inMatning.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main()
 {
+    
     ///Skriv in ett heltal
     int h;
+    /*
     cout << "Skriv in ett heltal: ";
     cin >> h;
     cout << "Du skrev in heltalet: " << h << "\n" << flush;
@@ -28,11 +30,33 @@ int main()
          cout << h << ", ";
         }
     } 
-
+    */
     ///Skriv in ett flyttal
     double f;
+    /*
     cout << "Skriv in ett flyttal: ";
     cin >> f;
-    cout << "Du skrev in: flyttalet: " << setfill(-) << setw(8) << setprecision(2) << f << "\n" << flush;
+    cout << "Du skrev in: flyttalet: " << setprecision(4) << f << "\n" << flush;
+    */
+
+    ///Skriv in ett heltal och ett flyttal
+    /*
+    cout << "Skriv in ett heltal och ett flyttal: ";
+    cin >> h >> f;
+    cout << "Du skrev in heltalet: " << setw(10) << h << "\n" << flush;
+    cout << "Du skrev in Flyttalet: " << setw(9) << setprecision(4) << f << "\n" << flush;
+
+    cout << "Skriv in ett flyttal och heltal: ";
+    cin >> f >> h;
+    cout << "Du skrev in heltalet: " << setfill('-') << setw(10) << h << "\n" << flush;
+    cout << "Du skrev in Flyttalet: " << setfill('-') << setw(9) << setprecision(4) << f << "\n" << flush;
+    */
+    char c;
+    /*
+    cout << "Skriv in ett tecken: ";
+    cin >> c;
+    cout << "Du skrev in tecknet: " << c << "\n" << flush;
+    */
+    
     return 0;
 }
