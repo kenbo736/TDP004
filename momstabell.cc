6faed6e5a655f9ad9e6203e62e7e36f667fe4571
Kenneth <kenbo736@student.liu.se>
2019-11-06 17:30:25 +0100

    Test

--- momstabell.cc
+++ momstabell.cc
@@ -10,7 +10,7 @@ int main() {
 
   cout << "INMATNINGSDEL" << endl;
   cout << "=============" << endl;
-  cout << "Mata in första pris: ";
+  cout << "Mata in första pris: TEST";
   cin >> x;
   cout << "Mata in sista pris: ";
   cin >> y;
