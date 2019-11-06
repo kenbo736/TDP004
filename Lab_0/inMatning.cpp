cf4635c43a29d4047c399aff51ef0e62656fa2c5
Kenneth <kenbo736@student.liu.se>
2019-11-06 18:27:44 +0100

    inmatning

--- Lab_0/inMatning.cpp
+++ Lab_0/inMatning.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+    ///Skriv in ett heltal
+    int h;
+    cout << "Skriv in ett heltal: ";
+    cin >> h;
+    cout << "Du skrev in heltalet: " << h << "\n" << flush;
 
+    ///Skriv in fem heltal
+    cout << "Skriv in fem heltal: ";
+    ///cout << "Du skrev in heltalen: " << "\n" << flush;
+    for(int i{0}; i<5; ++i)
+    {
+        cin >> h;
+        if(i == 0)
+        { 
+         cout << "Du skrev ut talen: " << h << ", ";
+        }
+        else if(i == 4)
+        {
+         cout << h << endl;
+        }
+        else
+        {
+         cout << h << ", ";
+        }
+    } 
+    return 0;
 }
