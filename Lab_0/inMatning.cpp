158df382f2d4d67994084bbaa49286ef544ed721
kenbo736 <kenbo736@roxen2-111.ad.liu.se>
2019-11-07 15:50:20 +0100

    Strängar och heltal

--- Lab_0/inMatning.cpp
+++ Lab_0/inMatning.cpp
@@ -8,7 +8,7 @@ int main()
 
     ///Skriv in ett heltal
     int h;
-    /*
+    
     cout << "Skriv in ett heltal: ";
     cin >> h;
     cout << "Du skrev in heltalet: " << h << "\n" << flush;
@@ -31,17 +31,17 @@ int main()
          cout << h << ", ";
         }
     }
-    */
+
     ///Skriv in ett flyttal
     double f;
-    /*
+
     cout << "Skriv in ett flyttal: ";
     cin >> f;
     cout << "Du skrev in: flyttalet: " << setprecision(4) << f << "\n" << flush;
-    */
+
 
     ///Skriv in ett heltal och ett flyttal
-    /*
+
     cout << "Skriv in ett heltal och ett flyttal: ";
     cin >> h >> f;
     cout << "Du skrev in heltalet: " << setw(10) << h << "\n" << flush;
@@ -51,16 +51,16 @@ int main()
     cin >> f >> h;
     cout << "Du skrev in heltalet: " << setfill('-') << setw(10) << h << "\n" << flush;
     cout << "Du skrev in Flyttalet: " << setfill('-') << setw(9) << setprecision(4) << f << "\n" << flush;
-    */
+
     char c;
-    /*
+
     cout << "Skriv in ett tecken: ";
     cin >> c;
     cout << "Du skrev in tecknet: " << c << "\n" << flush;
-    */
+
 
     string s;
-    /*
+
     cout << "Skriv in en sträng: ";
     cin >> s;
     cout << "Strängen '" << s << "' har " << s.length() << " tecken." << "\n" << flush;
@@ -68,13 +68,17 @@ int main()
     cout << "Skriv in ett heltal och en sträng: ";
     cin >> h >> s;
     cout << "Du skrev in talet |" << h << "| och strängen |" << s << "|. \n" << flush;
-    */
+
     cout << "Skriv in en sträng och ett flyttal: ";
     cin >> s >> f;
     cout << "Du skrev in '" << f << setprecision(3) << "' och '" << s << "'. \n" << flush;
 
     cout << "Skriv in hel rad med text: ";
+    cin.ignore(999, '\n');
     getline(cin, s);
     cout << "Du skrev in textraden: '" << s << "' \n" << flush;
+
+    cout << "Skriv in en textrad som slutar med ett negativt heltal: ";
+
     return 0;
 }
