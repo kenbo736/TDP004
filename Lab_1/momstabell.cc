915bd6aa489447603294d058c7b6fbedf50b879e
kenbo736 <kenbo736@su04-106.ad.liu.se>
2019-11-11 19:02:04 +0100

    problem with fillfunction

--- Lab_1/momstabell.cc
+++ Lab_1/momstabell.cc
@@ -2,7 +2,31 @@
 #include <iomanip>
 using namespace std;
 
-int main() 
+void momstabell(double i, double i2, double i3, double i4)
 {
-  
+  cout << "\nMOMSTABELL" << endl;
+  cout << "==========" << endl;
+  cout << setw(10) << "Pris" << setw(10) << "Moms" << setw(20) << "Pris med moms" << endl;
+  cout << setw(10) << setfill('-') << fixed << endl;
+}
+
+int main()
+{
+  double fp{};
+  double sp{};
+  double steg{};
+  double moms{};
+  cout << "INMATNINGSDEL" << endl;
+  cout << "=============" << endl;
+  cout << "Mata in första pris: " << endl;
+  cin >> fp;
+  cout << "Mata in sista pris: " << endl;
+  cin >> sp;
+  cout << "Mata in steglängd: " << endl;
+  cin >> steg;
+  cout << "Mata in momsprocent" <<  endl;
+  cin >> moms;
+
+  momstabell(fp, sp, steg, moms);
+  return 0;
 }
