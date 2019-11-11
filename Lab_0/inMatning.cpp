41a1318112c2cc8326735eaffc014349eef0dac9
kenbo736 <kenbo736@su04-106.ad.liu.se>
2019-11-11 18:30:06 +0100

    Problem with last line and the 12.346

--- Lab_0/inMatning.cpp
+++ Lab_0/inMatning.cpp
@@ -7,14 +7,15 @@ int main()
 {
 
     ///Skriv in ett heltal
-    int h;
-    
-    cout << "Skriv in ett heltal: ";
+    int h{};
+
+    cout << "Skriv in ett heltal: " << endl;
     cin >> h;
+    cin.ignore(999, '\n');
     cout << "Du skrev in heltalet: " << h << "\n" << flush;
 
     ///Skriv in fem heltal
-    cout << "Skriv in fem heltal: ";
+    cout << "Skriv in fem heltal: " << endl;
     for(int i{0}; i<5; ++i)
     {
         cin >> h;
@@ -31,54 +32,57 @@ int main()
          cout << h << ", ";
         }
     }
-
+    cin.ignore(999, '\n');
     ///Skriv in ett flyttal
-    double f;
+    double f{};
 
-    cout << "Skriv in ett flyttal: ";
+    cout << "Skriv in ett flyttal: " << endl;
     cin >> f;
-    cout << "Du skrev in: flyttalet: " << setprecision(4) << f << "\n" << flush;
+    cout << "Du skrev in: flyttalet: " << setprecision(3) << fixed << f << "\n" << flush;
 
-
+    cin.ignore(999, '\n');
     ///Skriv in ett heltal och ett flyttal
-
-    cout << "Skriv in ett heltal och ett flyttal: ";
+    cout << "Skriv in ett heltal och ett flyttal: " << endl;
     cin >> h >> f;
     cout << "Du skrev in heltalet: " << setw(10) << h << "\n" << flush;
     cout << "Du skrev in Flyttalet: " << setw(9) << setprecision(4) << f << "\n" << flush;
 
-    cout << "Skriv in ett flyttal och heltal: ";
+    cin.ignore(999, '\n');
+    cout << "Skriv in ett flyttal och heltal: " << endl;
     cin >> f >> h;
     cout << "Du skrev in heltalet: " << setfill('-') << setw(10) << h << "\n" << flush;
     cout << "Du skrev in Flyttalet: " << setfill('-') << setw(9) << setprecision(4) << f << "\n" << flush;
+    cin.ignore(999, '\n');
+    char c{};
 
-    char c;
-
-    cout << "Skriv in ett tecken: ";
+    cout << "Skriv in ett tecken: " << endl;
     cin >> c;
     cout << "Du skrev in tecknet: " << c << "\n" << flush;
+    cin.ignore(999, '\n');
 
+    string s{};
 
-    string s;
-
-    cout << "Skriv in en sträng: ";
+    cout << "Skriv in en sträng: " << endl;
     cin >> s;
     cout << "Strängen '" << s << "' har " << s.length() << " tecken." << "\n" << flush;
 
-    cout << "Skriv in ett heltal och en sträng: ";
+    cout << "Skriv in ett heltal och en sträng: " << endl;
     cin >> h >> s;
     cout << "Du skrev in talet |" << h << "| och strängen |" << s << "|. \n" << flush;
 
-    cout << "Skriv in en sträng och ett flyttal: ";
+    cin.ignore(999, '\n');
+    cout << "Skriv in en sträng och ett flyttal: " << endl;
     cin >> s >> f;
     cout << "Du skrev in '" << f << setprecision(3) << "' och '" << s << "'. \n" << flush;
 
-    cout << "Skriv in hel rad med text: ";
+    cout << "Skriv in hel rad med text: " << endl;
     cin.ignore(999, '\n');
     getline(cin, s);
     cout << "Du skrev in textraden: '" << s << "' \n" << flush;
 
-    cout << "Skriv in en textrad som slutar med ett negativt heltal: ";
+    cout << "Skriv in en textrad som slutar med ett negativt heltal: " << endl;
 
+
+    cout << "Du skrev in textraden: '" << s << "' och heltalet '" << h << "'" << endl;
     return 0;
 }
