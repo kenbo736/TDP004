041ac33993fa4036c0f0f0ac340b2a8259312552
kenbo736 <kenbo736@tlhw-4-1.ad.liu.se>
2019-11-12 18:06:46 +0100

    addad in time.h

--- Lab_2/given_files/Time.cc
+++ Lab_2/given_files/Time.cc
@@ -1,31 +1,38 @@
+// I denna fil läggs definitionerna (implementationen) av de funktioner
+// som deklarerats i Time.h
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <unistd.h>
+#include "Time.h"
 
 using namespace std;
-// I denna fil läggs definitionerna (implementationen) av de funktioner
-// som deklarerats i Time.h
-struct times {
-  int hh{};
-  int mm{};
-  int ss{};
-};
-
-void to_string(times); //function
 
 int main(){
-  times t1{2, 2, 3 + 5};
+  times t1{};
   times t2{};
 
-  cout << setfill('0') << setw(2) << t1.hh << ":" << setfill('0') << setw(2) << t1.mm << ":" << setfill('0') << setw(2) << t1.ss << endl;
-
   to_string(t1);
+  to_string(t2);
   return 0;
 }
 
 void to_string(times t){
   if (t.hh<24 && t.mm<60 && t.ss<60){
-    cout << "Test" << endl;
+    cout << setfill('0') << setw(2) << t.hh << ":" << setfill('0') << setw(2) << t.mm << ":" << setfill('0') << setw(2) << t.ss << endl;
   }
 }
+
+/*
+if (t.hh<24 && t.mm<60 && t.ss<60){
+  for(int h = 0; h < 24; h++){
+    for(int m = 0; m < 60; m++){
+      for(int s = 0; s < 60; s++){
+        cout << setfill('0') << setw(2) << t.hh+h << ":" << setfill('0') << setw(2) << t.mm+m << ":" << setfill('0') << setw(2) << t.ss+s << endl;
+        usleep(1000);
+      }
+    }
+  }
+}
+*/
--- Lab_2/given_files/Time.h
+++ Lab_2/given_files/Time.h
@@ -1,2 +1,10 @@
 // Denna fil ska innehålla deklarationer för de typer och funktioner
 // som behövs
+
+struct times {
+  int hh{};
+  int mm{};
+  int ss{};
+};
+
+void to_string(times); //function
